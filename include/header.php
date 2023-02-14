7e067c9b0c61ee50ce49f4f1b44225440a33cda2
mehdi-romdhani <mehdi.romdhani@laplateforme.io>
2023-02-14 15:46:32 +0100

    mise en place interface

---
+++ include/header.php
@@ -0,0 +1,11 @@
+<div class="container-nav">
+    <header>
+        <ul>
+            <li>Livre d'or</li>
+            <li><a href="#">Connexion</a></li>
+            <li><a href="#">Inscription</a></li>
+    
+        </ul>
+    </header>
+
+</div>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!-- Projet : Livre d'or 
+Langages : HTML / CSS / JS / PHP
+Auteur : Mehdi Romdhani
+Date : 14/02/2023 -->
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINK CSS  -->
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Livre d'or _ Version 2 </title>
+</head>
+<body>
+<!-- **********************HEADER NAV ****************** -->
+<?php require_once './include/header.php' ?>
+
+    
+</body>
+</html>
